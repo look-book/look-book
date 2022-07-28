39de80c6d5f9c5dc02332464187bf273fa5ddf20
Vasilyg10 <zvgianna@gmail.com>
2022-07-28 18:41:10 -0500

    added concurrently and got server and client running

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
+const path = require("path");
 const express = require("express");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+// const routes = require("./routes");
+
 const app = express();
-require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -14,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
@@ -24,8 +25,6 @@ mongoose.connect(
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false
     }
   );
   
