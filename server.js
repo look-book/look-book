5ee78fbb8e4da4812688566996718ee92ca22fe2
Charlene <charlene@peters-services.com>
2022-07-28 16:37:26 -0500

    created build

--- server.js
+++ server.js
@@ -5,9 +5,4 @@ const port = process.env.PORT || 5000; //Line 3
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
-// create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
-
-//https://www.section.io/engineering-education/how-to-setup-nodejs-express-for-react/
+app.use(express.static(__dirname + '/Client/public'));
