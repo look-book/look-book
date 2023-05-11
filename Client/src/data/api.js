9dc93c1e32514f892a118fbc506230d5578f0d96
ALMA_DEV914 <aungonalna58@gamil.com>
2023-05-11 03:11:30 -0700

    updates api routes for uploads

--- Client/src/data/api.js
+++ Client/src/data/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:5000/api/' || 'https://look-book-act-group42.herokuapp.com/';
+const apiUrl = process.env.REACT_APP_PUBLIC_FOLDER;
 
 export const singleFileUpload = async (data, options) => {
     try {
