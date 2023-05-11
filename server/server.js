8a4ffd3fbf9e220af9c580146ca2dea1b7d95f51
ALMA_DEV914 <aungonalna58@gamil.com>
2023-05-11 01:47:56 -0700

    update json

--- server/server.js
+++ server/server.js
@@ -88,7 +88,7 @@ app.use('/api', fileRoutes.routes);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 if (process.env.NODE_ENV === 'production') {
