b582c6f11d9a577f859d11ccd7c018d9f25cddd6
ALMA_DEV914 <aungonalna58@gamil.com>
2023-05-11 12:36:25 -0700

    update client

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const path = require("path");
 
 //defining mongoose options
 const corsOptions = {
-  origin: "http://locahost:3000" || "https://look-book-act-group42.herokuapp.com/",
+  origin: "http://locahost:3000" || "https://look-book-act-group42.herokuapp.com",
   credentials: true,
   optionSuccessStatus: 200,
   header: {
