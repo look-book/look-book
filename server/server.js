37f644f3ea45d568db1e621d8436929d90a4c42c
ALMA_DEV914 <aungonalna58@gamil.com>
2023-05-12 00:52:22 -0700

    update build files for client

--- server/server.js
+++ server/server.js
@@ -82,27 +82,10 @@ app.use("/posts", postRoutes);
 
 app.use('/api', fileRoutes.routes);
 
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../Client/build')));
-}
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Client/build/index.html'));
-});
-
-
-if(process.env.NODE_ENV=="production"){
-  app.use(express.static('client/build'))
-
-  app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-
 }
 
 app.get('*', (req, res) => {
