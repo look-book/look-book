70360958070b328a855fa1630de56eae70ce1a8e
Alma Braun <aungonalna58@gmail.com>
2023-05-13 00:05:56 -0700

    Update post.js

--- Client/src/api/post.js
+++ Client/src/api/post.js
@@ -1,6 +1,20 @@
 import axios from 'axios';
 
 const url =  'http://localhost:5000/posts' || 'https://look-book-act-group42.herokuapp.com/';
+//defining  options
+const options = {
+  origin: ["http://localhost:5000/posts", "https://look-book-act-group42.herokuapp.com/"],
+   mode: "cors",
+  credentials: "include",
+  optionSuccessStatus: 200,
+  header: {
+    "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com",
+    "Access-Control-Allow-Origin": true,
+    "Access-Control-Allow-Private-Network": true,
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+};
+
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
