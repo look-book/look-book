03fd925b6bc57e947ae6eebf2341bbb3ea92b65b
Alma Braun <aungonalna58@gmail.com>
2023-05-13 11:34:32 -0700

    Update FileUploadScreen.js

--- Client/src/screens/FileUploadScreen.js
+++ Client/src/screens/FileUploadScreen.js
@@ -32,7 +32,7 @@ const FileUploadScreen = (props) => {
           Accept: "application/json",
           "Content-Type": "application/json",
           "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com/",
         },
       })
         .then((response) => {
