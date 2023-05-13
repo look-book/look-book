6aedb7623aae0b71aa530e2a6f99dee05515fb41
Alma Braun <aungonalna58@gmail.com>
2023-05-13 12:38:54 -0700

    Update FileUploadScreen.js

--- Client/src/screens/FileUploadScreen.js
+++ Client/src/screens/FileUploadScreen.js
@@ -25,14 +25,14 @@ const FileUploadScreen = (props) => {
 
   useEffect(() => {
     const getUser = () => {
-      fetch("http://localhost:5000/auth/login/success", {
+      fetch("/auth/login/success", {
         method: "GET",
         credentials: "include",
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
           "Access-Control-Allow-Credentials": true,
-          "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com/",
+          "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com",
         },
       })
         .then((response) => {
