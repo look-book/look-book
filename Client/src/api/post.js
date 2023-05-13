5e9112c1c4290848bfe03a2f625ff5f89370c8b3
Alma Braun <aungonalna58@gmail.com>
2023-05-12 21:50:39 -0700

    Update post.js
    Update url posts

--- Client/src/api/post.js
+++ Client/src/api/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url =  "http://localhost:5000/posts";
+const url =  'http://localhost:5000/posts' || 'https://look-book-act-group42.herokuapp.com/';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
