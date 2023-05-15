1620a8922478726716bdbd6851dee20bf7b99787
Alma Braun <aungonalna58@gmail.com>
2023-05-15 08:00:25 -0700

    Update api.js

--- Client/src/data/api.js
+++ Client/src/data/api.js
@@ -3,14 +3,12 @@ import axios from 'axios';
 const apiUrl =  'http://localhost:5000/api/' || 'https://look-book-act-group42.herokuapp.com/api/';
 
 const options = {
-  origin: ["http://localhost:3000" , "https://look-book-act-group42.herokuapp.com/"],
-  preflightContinue:false,
-  credentials: true,
+   method: "GET",
+  credentials: "include"
   optionSuccessStatus: 200,
   header: {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Origin": true,
-    "Access-Control-Allow-Private-Network": true,
     "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
   },
 };
