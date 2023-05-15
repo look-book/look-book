13532057e29f980da2bad132089a5725fabcd39b
Alma Braun <aungonalna58@gmail.com>
2023-05-15 01:57:07 -0700

    Update post.js

--- Client/src/api/post.js
+++ Client/src/api/post.js
@@ -1,11 +1,24 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts' || 'https://look-book-act-group42.herokuapp.com/posts';
+const url ='https://look-book-act-group42.herokuapp.com/posts';
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
+const options = {
+  origin: ["http://localhost:3000" , "https://look-book-act-group42.herokuapp.com/"],
+  preflightContinue:false,
+  credentials: true,
+  optionSuccessStatus: 200,
+  header: {
+    "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com" || "http://localhost:5000/posts",
+    "Access-Control-Allow-Origin": true,
+    "Access-Control-Allow-Private-Network": true,
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
+};
+
+export const fetchPosts = () => axios.get(url, options);
+export const createPost = (newPost) => axios.post(url, newPost, options);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`, options);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost, options);
+export const deletePost = (id) => axios.delete(`${url}/${id}`, options);
 
 
