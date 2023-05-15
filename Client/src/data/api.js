e5408950f704d5d49868b7ab0e05316039a21cdc
Alma Braun <aungonalna58@gmail.com>
2023-05-15 12:15:26 -0700

    Update api.js

--- Client/src/data/api.js
+++ Client/src/data/api.js
@@ -5,7 +5,7 @@ const apiUrl =  'http://localhost:5000/api/' || 'https://look-book-act-group42.h
 const options = {
   credentials: "include",
   optionSuccessStatus: 200,
-  mode: "no-cors",
+  mode: "cors",
   header: {
     "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com",
     "Access-Control-Allow-Origin": true,
