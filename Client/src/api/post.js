34d8831ed02db6766ce7f385b8c95e7184b9cb8b
ALMA_DEV914 <aungonalna58@gamil.com>
2023-05-15 18:32:48 -0700

    updates

--- Client/src/api/post.js
+++ Client/src/api/post.js
@@ -1,20 +1,12 @@
 import axios from 'axios';
 
-const url = "http://localhost:5000/posts";
+const url =  "https://look-book-act-group42.herokuapp.com/posts" || "http://localhost:5000/posts";
 
-const options = {
-    header: {
-      "Access-Control-Allow-Origin": "https://look-book-act-group42.herokuapp.com/",
-      "Content-type":"application/json",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    },
-  };
   
-
-export const fetchPosts = () => axios.get(url, options);
-export const createPost = (newPost) => axios.post(url, newPost, options );
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`, options);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost, options );
-export const deletePost = (id) => axios.delete(`${url}/${id}`, options);
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`, );
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
 
 
