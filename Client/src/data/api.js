e33afb43ee516ce3e6f6edc674fa3fe94fb2d4c7
Charlene Peters <cpeters1402@gmail.com>
2023-05-19 20:19:50 -0500

    Update api.js

--- Client/src/data/api.js
+++ Client/src/data/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 //"http://localhost:5000/api/" || 
-const apiUrl = "https://look-book-act-group42.herokuapp.com/api/";
+const apiUrl = "//look-book-act-group42.herokuapp.com/api/";
 
 
 export const singleFileUpload = async (data, options) => {
