869a21e4e4999e7137c83395992e24e23902b98e
Charlene Peters <cpeters1402@gmail.com>
2023-05-19 19:30:54 -0500

    Update Login.js

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -49,11 +49,11 @@ function Login() {
   }, [history]);
 
   const onGoogle = () => {
-    window.open("http://localhost:5000/auth/google", "_self");
+    window.open("https://look-book-act-group42.herokuapp.com/auth/google", "_self");
   };
 
   const onFacebook = () => {
-    window.open("http://localhost:5000/auth/facebook", "_self");
+    window.open("https://look-book-act-group42.herokuapp.com/auth/facebook", "_self");
   };
 
   return (
