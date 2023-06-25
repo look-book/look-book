f0db6ecdd5456e3154851a08c66e72f32b775c33
Alma Braun <aungonalna58@gmail.com>
2023-06-25 02:41:58 -0700

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -30,9 +30,10 @@ import Profile from "./components/Profile";
 function App(props) {
   return (
     <div className="App">
+     <Navbar props="user" />
       <Router>
         <Routes>
-          <Navbar props="user" />
+         
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/register" element={<Register />} />
