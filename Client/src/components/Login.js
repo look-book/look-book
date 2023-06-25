17eabba0199aa1edd4f992af10947aec82b42212
ALMA_DEV914 <aungonalna58@gamil.com>
2023-06-25 15:42:21 -0700

    updates

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; 
 import { Link, useNavigate } from "react-router-dom";
 import Google from "../assets/google.png";
 import Facebook from "../assets/facebook.png";
