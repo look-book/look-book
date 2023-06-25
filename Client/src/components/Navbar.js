edad1730c017878f59f13ad3099d9efea6e053d3
Alma Braun <aungonalna58@gmail.com>
2023-06-25 01:15:58 -0700

    Update Navbar.js

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import avatar from "../assets/profile.jpg";
 import logo from "../assets/logo.png";
 
 
-function Navbar({user) {
+function Navbar({user}) {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
