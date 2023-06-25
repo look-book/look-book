9f2224721c04dc7f20b37e9abb2cf4e54aeb362c
Alma Braun <aungonalna58@gmail.com>
2023-06-25 00:31:32 -0700

    Update ValidationError.js

--- Client/src/components/ValidationError.js
+++ Client/src/components/ValidationError.js
@@ -1,6 +1,6 @@
 function ValidationError({message}) {
     return (
-        <div className="my-5 transition-opacity-100 text-center text-xl text-red-500 p-2 font-extrabold">{message}</div>
+        <div className="errmsg">{message}</div>
     )
 }
 
