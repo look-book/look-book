6d587ffcde3d763ba9e698696add73e5f6df9b85
Alma Braun <aungonalna58@gmail.com>
2023-06-26 13:20:17 -0700

    Update Login.js

--- Client/src/components/Login.js
+++ Client/src/components/Login.js
@@ -43,7 +43,7 @@ function Login() {
       },
     })
       .then((res) => res.json())
-      .then((data) =>  .then((data) => (data.isLoggedIn ? setUser(data) : null))
+      .then((data) => (data.isLoggedIn ? setUser(data) : null))
       .catch((err) => console.log(err));
   }, [history]);
 
