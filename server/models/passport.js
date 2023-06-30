9f20c5d600c4948d02496b001bb6e86b9c764051
Alma Braun <aungonalna58@gmail.com>
2023-06-30 16:06:53 -0700

    Update passport.js

--- server/models/passport.js
+++ server/models/passport.js
@@ -27,16 +27,6 @@ passport.use(
   )
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id).then((user) => {
-    done(null, user);
-  });
-});
-
 // For facebook
 passport.use(
   new FacebookStrategy(
@@ -45,28 +35,9 @@ passport.use(
       clientSecret: process.env.FACEBOOK_APP_SECRET,
       callbackURL: "/auth/facebook/callback"
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
-      /* ========= DATABASE CHECK PRE EXIST AND INSERT QUERY: START =========  */
-      // check if user id already inserted
-      User.findOne({ userId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          // new user case
-          // insert new user id
-          new User({
-            userId: profile.id,
-            username: profile.displayName,
-            picture: profile._json.picture
-          })
-            .save()
-            .then(user => {
-              done(null, user);
-            });
-        }
-      });
-      /* ========= DATABASE CHECK PRE EXIST AND INSERT QUERY: END =========  */
+    (accessToken, refreshToken, profile, callback) => {
+      callback(null, profile, accessToken, refreshToken);
+     
     }
   )
-);
+); 
