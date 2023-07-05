57dbf0a64133758b119e56c791b850ed7ab5e778
Alma Braun <aungonalna58@gmail.com>
2023-07-05 14:07:14 -0700

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -38,10 +38,7 @@ router.get(
   "/facebook",
   passport.authenticate("facebook", { scope: ["profile", "email"] })
 );
-router.get(
-  "/auth/facebook",
-  passport.authenticate("facebook", { scope: ["profile", "email"] })
-);
+
 
 router.get(
   "/facebook/callback",
@@ -50,46 +47,5 @@ router.get(
     failureRedirect: "/login/failed",
   })
 );
-
-router.post('/auth/facebook', async (req, res) => {
-  try {
-    const { userId = '', accessToken = '' } = req.body;
-    if (userId === '' || accessToken === '') {
-      return res.status(400).json({ message: "userId and accessToken are required" });
-    }
-
-    // Get user by Facebook userId and accessToken
-    let { data } = await getUserByFacebookIdAndAccessToken(accessToken, userId);
-
-    // Check if user exists
-    var user = await User.findOne({ facebookId: data.id });
-    var authObject = {};
-
-    if (user) {
-      var token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '20h' });
-      authObject = { auth: true, token, user, message: "Successfully logged in." };
-      return res.status(201).json(authObject);
-    } else {
-      user = await User.create({
-        name: data.name,
-        email: data.email,
-        facebookId: data.id
-      });
-
-      var token = jwt.sign({ id: user._id }, 'secret', { expiresIn: '20h' });
-      authObject = { auth: true, token, user, message: "Successfully Registered." };
-      return res.status(201).json(authObject);
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: error.message });
-  }
-});
-
-let getUserByFacebookIdAndAccessToken = (accessToken, userId) => {
-  let urlGraphFacebook = `https://graph.facebook.com/v17.0/${userId}?fields=id,name,email&access_token=${accessToken}`;
-  let result = axios.get(urlGraphFacebook);
-  return result;
-};
 
 module.exports = router;
