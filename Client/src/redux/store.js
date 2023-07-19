34ae59fbdb70f46d4b0cdccae1f96461dd4ce735
Alma Braun <aungonalna58@gmail.com>
2023-07-19 11:56:15 -0700

    Update store.js

--- Client/src/redux/store.js
+++ Client/src/redux/store.js
@@ -5,23 +5,16 @@ import questionReducer from "./question_reducer";
 import resultReducer from "./result_reducer";
 import posts from "./posts";
 import uploads from "./uploads";
-import { authReducer } from './authReducer';
+
 
 const rootReducer = combineReducers({
   questions: questionReducer,
   result: resultReducer,
   posts: posts,
   uploads: uploads,
-  authReducer: authReducer
-
 });
 
-const userInfoFromStorage = localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo'))
-  : null;
-
-const initialState = { userLogin: { userInfo: userInfoFromStorage } };
 
 
 /** create store with reducer */
-export default configureStore({ reducer: rootReducer, initialState });
+export default configureStore({ reducer: rootReducer});
